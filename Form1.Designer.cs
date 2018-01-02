ab2e5730fc193dffd9e56e1b5573d8126a5ce103
Singlerr <singerr@naver.com>
2018-01-02 17:14:34 +0900

    c

---
+++ Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace Catholic
+{
+    partial class main
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(main));
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button1 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.path = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.st = new System.Windows.Forms.TextBox();
+            this.sn = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.en = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 108);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(220, 23);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(142, 9);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(98, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "다운로드 시작";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(12, 12);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(47, 16);
+            this.radioButton1.TabIndex = 2;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "알토";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(65, 12);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(71, 16);
+            this.radioButton2.TabIndex = 3;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "소프라노";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(12, 34);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(47, 16);
+            this.radioButton3.TabIndex = 4;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "테너";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(65, 34);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(59, 16);
+            this.radioButton4.TabIndex = 5;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "베이스";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.CheckedChanged += new System.EventHandler(this.radioButton4_CheckedChanged);
+            // 
+            // path
+            // 
+            this.path.Location = new System.Drawing.Point(12, 81);
+            this.path.Name = "path";
+            this.path.ReadOnly = true;
+            this.path.Size = new System.Drawing.Size(139, 21);
+            this.path.TabIndex = 6;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(157, 79);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "...";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // st
+            // 
+            this.st.Location = new System.Drawing.Point(75, 56);
+            this.st.Name = "st";
+            this.st.Size = new System.Drawing.Size(47, 21);
+            this.st.TabIndex = 8;
+            // 
+            // sn
+            // 
+            this.sn.AutoSize = true;
+            this.sn.Location = new System.Drawing.Point(10, 59);
+            this.sn.Name = "sn";
+            this.sn.Size = new System.Drawing.Size(63, 12);
+            this.sn.TabIndex = 9;
+            this.sn.Text = "시작(번호)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(128, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 12);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "끝(번호)";
+            // 
+            // en
+            // 
+            this.en.Location = new System.Drawing.Point(185, 56);
+            this.en.Name = "en";
+            this.en.Size = new System.Drawing.Size(47, 21);
+            this.en.TabIndex = 11;
+            // 
+            // main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(244, 143);
+            this.Controls.Add(this.en);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.sn);
+            this.Controls.Add(this.st);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.path);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.progressBar1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "main";
+            this.Text = "성가 내려받기";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.TextBox path;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox st;
+        private System.Windows.Forms.Label sn;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox en;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+namespace Catholic
+{
+    public partial class main : Form
+    {
+        private static string type;
+        public main()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+         
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folder = new FolderBrowserDialog();
+            if(folder.ShowDialog() == DialogResult.OK)
+            {
+                path.Text = folder.SelectedPath;
+            }
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            type = "alt";
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            type = "sop";
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            type = "ten";
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+            type = "bas";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(path.Text != null)
+            {
+                int s, n;
+                if(radioButton1.Checked || radioButton2.Checked || radioButton3.Checked || radioButton4.Checked)
+                {
+                    if( int.TryParse(en.Text,out n) && int.TryParse(st.Text,out s))
+                    {
+                        if(s < n)
+                        {
+                            DownloadManager download = new DownloadManager(path.Text, type, s, n, progressBar1);
+                            download.start();
+                          
+                        }
+                        else
+                        {
+                            MessageBox.Show("시작 번호가 끝 번호보다 큽니다.", "오류", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                       
+                    }
+                    else
+                    {
+                        MessageBox.Show("시작 번호와 끝 번호가 숫자가 아니거나 입력하지 않았습니다.", "오류", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("알토,소프라노,테너,베이스 중 한가지를 선택해주십시오.", "오류", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("저장 경로를 설정해 주십시오.(\"...\" 버튼)", "오류", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+    public class DownloadManager
+    {
+        public static string SEPERATOR = "/";
+        public static string FILE_SEPERATOR = @"\";
+        public static string PARENT_URL = "https://www.mariasarang.net/files/music_catholic";
+        private string path;
+        private string t;
+        private Timer async;
+        private WebClient webClient;
+        private Queue<string> queue;
+        private ProgressBar progressBar;
+  
+        public DownloadManager(string path,string type,int from,int to,ProgressBar progressBar)
+        {
+         
+            webClient = new WebClient();
+            this.progressBar = progressBar;
+            queue = new Queue<string>();
+            this.path = path;
+            if(path == null)
+            {
+                MessageBox.Show("파일 경로가 필요합니다","오류",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                return;
+            }
+            t = type;
+            if (type != null)
+            {
+                t = type.ToLower();
+                for (int i = from; i < to; i++)
+                {
+                    queue.Enqueue(PARENT_URL + SEPERATOR + "part" + SEPERATOR + t + SEPERATOR + i.ToString("000") + t.ToCharArray()[0] + ".mid"+"|"+path+FILE_SEPERATOR + i.ToString("000") + t.ToCharArray()[0] + ".mid");
+                }
+            }
+            async = new Timer();
+        }
+        public void start()
+        {
+            if (!Directory.Exists(path))
+                Directory.CreateDirectory(path);
+
+         
+                async.Tick += new EventHandler(download);
+                progressBar.Maximum = queue.Count;
+                async.Interval = 50;
+                async.Start();
+           
+          
+        }
+        private void download(object o,EventArgs e)
+        {
+                if(queue.Count != 0)
+            {
+                string data = queue.Dequeue();
+                webClient.DownloadFile(data.Split('|')[0], data.Split('|')[1]);
+                progressBar.Increment(1);
+            }
+            else
+            {
+                async.Stop();
+                MessageBox.Show("다운로드가 완료되었습니다.", "", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                progressBar.Value = 0;
+            }
+      
+        }
+    }
+    
+}
